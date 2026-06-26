3e9b6b604d795a32134df9fbba63e96ccc719880
Shihab Ahmed <shehabuddin380@gmail.com>
2026-06-27 02:01:40 +0600

    fix urls

--- api/urls.py
+++ api/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('orders/has-ordered/<int:product_id>/', HasOrderedView.as_view(), name='has-ordered'),
-    path('orders/<int:order_id>/payment/initiate/', StripeCheckoutView.as_view(), name='payment-initiate'),
+    path('orders/<uuid:order_id>/payment/initiate/', StripeCheckoutView.as_view(), name='payment-initiate'),
 
 ]
